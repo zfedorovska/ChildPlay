a574b8b66103c6ea4d84df14f75009108d661825
Zoryana <zfedorovska@gmail.com>
2020-04-06 00:33:13 +0300

    adjustments for mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
+    <h1 class='navbar-brand' >{{pageTitle}}</h1>
     <nav class='navbar navbar-expand navbar-light bg-light'>
-      <a class='navbar-brand'>{{pageTitle}}</a>
       <ul class='navbar-nav'>
         <li class='nav-item'><a class='nav-link' routerLinkActive='active'
               [routerLink]="['/welcome']">Home</a>
